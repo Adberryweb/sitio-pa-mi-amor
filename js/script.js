ce385b816829a1f166f1abbfdfdb024c0ad57344
Adberryweb <145831258+Adberryweb@users.noreply.github.com>
2024-06-28 14:09:59 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heartContainer = document.querySelector('.heart-container');
+    const createHeart = () => {
+        const heart = document.createElement('div');
+        heart.classList.add('heart');
+        heart.style.left = `${Math.random() * 100}%`;
+        heart.style.animationDuration = `${Math.random() * 3 + 2}s`; // Duración entre 2 y 5 segundos
+        heartContainer.appendChild(heart);
 
+        setTimeout(() => {
+            heart.remove();
+        }, 5000); // Remueve el corazón después de 5 segundos
+    }
+
+    setInterval(createHeart, 300);
+});
